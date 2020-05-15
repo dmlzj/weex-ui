e2007be12192d84aa64df341f0842fb436f0e6ea
dmlzj <284832506@qq.com>
2020-05-15 09:16:27 +0800

    fix element.ref undefined

--- packages/wxc-pan-item/index.vue
+++ packages/wxc-pan-item/index.vue
@@ -22,59 +22,61 @@
 </template>
 
 <script>
-  import Binding from 'weex-bindingx/lib/index.weex.js';
-  import Utils from '../utils';
-  import BindEnv from '../utils/bind-env';
+ import Binding from 'weex-bindingx/lib/index.weex.js';
+ import Utils from '../utils';
+ import BindEnv from '../utils/bind-env';
 
-  export default {
-    props: {
-      url: {
-        type: String,
-        default: ''
-      },
-      needSlider: {
-        type: Boolean,
-        default: true
-      },
-    },
-    data: () => ({
-      isPanning: false,
-      appearMap: [],
-      supportAndroid: BindEnv.supportsEBForAndroid()
-    }),
-    mounted () {
-      setTimeout(() => {
-        if (this.supportAndroid && this.needSlider) {
-          const element = this.$refs['wxc-pan-item'];
-          Binding.prepare && Binding.prepare({
-            anchor: element.ref,
-            eventType: 'pan'
-          });
-        }
-      }, 300)
-    },
-    methods: {
-      itemClicked () {
-        if (this.isPanning) {
-          return;
-        }
-        this.url && Utils.goToH5Page(this.url, true);
-        this.$emit('wxcPanItemClicked', { extId: this.extId });
-      },
+ export default {
+   props: {
+     url: {
+       type: String,
+       default: ''
+     },
+     needSlider: {
+       type: Boolean,
+       default: true
+     },
+   },
+   data: () => ({
+     isPanning: false,
+     appearMap: [],
+     supportAndroid: BindEnv.supportsEBForAndroid()
+   }),
+   mounted () {
+     setTimeout(() => {
+       if (this.supportAndroid && this.needSlider) {
+         const element = this.$refs['wxc-pan-item'];
+	 if (element && element.ref) {
+	   Binding.prepare && Binding.prepare({
+             anchor: element.ref,
+             eventType: 'pan'
+           });
+	 }
+       }
+     }, 300)
+   },
+   methods: {
+     itemClicked () {
+       if (this.isPanning) {
+         return;
+       }
+       this.url && Utils.goToH5Page(this.url, true);
+       this.$emit('wxcPanItemClicked', { extId: this.extId });
+     },
 
-      dispatchPan (e) {
-        if (this.supportAndroid && this.needSlider) {
-          if (e.state === 'start') {
-            this.isPanning = true;
-            const element = this.$refs['wxc-pan-item'];
-            element && this.$emit('wxcPanItemPan', { element });
-          } else if (e.state === 'end') {
-            setTimeout(() => {
-              this.isPanning = false;
-            }, 50);
-          }
-        }
-      }
-    }
-  };
+     dispatchPan (e) {
+       if (this.supportAndroid && this.needSlider) {
+         if (e.state === 'start') {
+           this.isPanning = true;
+           const element = this.$refs['wxc-pan-item'];
+           element && this.$emit('wxcPanItemPan', { element });
+         } else if (e.state === 'end') {
+           setTimeout(() => {
+             this.isPanning = false;
+           }, 50);
+         }
+       }
+     }
+   }
+ };
 </script>
